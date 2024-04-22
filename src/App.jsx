dc56589ef6c77926104f1cae0fcaad711350357c
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2024-04-22 11:08:27 +0600

    Show fallback content when no project is clicked or created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { NewProject } from "./components/NewProject";
 import { ProjectsSidebar } from "./components/ProjectsSidebar";
+import { NoProjectSelected } from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectSate] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectSate((prevProjectsState) => {
+      return { ...prevProjectsState, selectedProjectId: null };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === undefined)
+    content = <NoProjectSelected onAddProject={handleAddProject} />;
+  else if (projectsState.selectedProjectId === null) content = <NewProject />;
+  
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onAddProject={handleAddProject} />
+      {content}
     </main>
   );
 }
